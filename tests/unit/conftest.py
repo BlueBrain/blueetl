2406a1cc9df1240e783c8ae03a12d5d76a8e7433
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2024-03-25 17:08:50 +0100

    Improve test coverage (#29)

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from blueetl.campaign.config import SimulationCampaign
 from blueetl.config.analysis import init_multi_analysis_configuration
+from blueetl.config.analysis_model import FeaturesConfig
+from blueetl.features import FeaturesCollection
 from blueetl.repository import Repository
 from blueetl.utils import load_yaml
 from tests.unit.utils import TEST_DATA_PATH, PicklableMock
@@ -23,11 +25,15 @@ def global_config():
 def repo(global_config):
     simulations_config = SimulationCampaign.load(global_config.simulation_campaign)
     extraction_config = global_config.analysis["spikes"].extraction
-    cache_manager = PicklableMock(load_repo=PicklableMock(return_value=None))
+    cache_manager = PicklableMock(
+        load_repo=PicklableMock(return_value=None),
+        load_features=PicklableMock(return_value=None),
+        get_cached_features_checksums=PicklableMock(return_value={}),
+    )
     simulations_filter = global_config.simulations_filter
     resolver = PicklableMock()
 
-    repo = Repository(
+    return Repository(
         simulations_config=simulations_config,
         extraction_config=extraction_config,
         cache_manager=cache_manager,
@@ -35,10 +41,50 @@ def repo(global_config):
         resolver=resolver,
     )
 
-    assert isinstance(repo, Repository)
-    assert repo.extraction_config == extraction_config
-    assert repo.simulations_config == simulations_config
-    assert repo.cache_manager == cache_manager
-    assert repo.simulations_filter == simulations_filter
-    assert repo.resolver == resolver
-    return repo
+
+@pytest.fixture
+def features(repo):
+    features_configs = [
+        FeaturesConfig(
+            type="multi",
+            groupby=["simulation_id", "circuit_id", "neuron_class", "window"],
+            function="blueetl.external.bnac.calculate_features.calculate_features_multi",
+            params={"export_all_neurons": True},
+            neuron_classes=["L2_X", "L6_Y"],
+            windows=["w0", "w1"],
+        )
+    ]
+    return FeaturesCollection(
+        features_configs=features_configs,
+        repo=repo,
+        cache_manager=repo.cache_manager,
+    )
+
+
+@pytest.fixture
+def features_with_suffixes(repo):
+    features_configs = [
+        FeaturesConfig(
+            type="multi",
+            groupby=["simulation_id", "circuit_id", "neuron_class", "window"],
+            function="blueetl.external.bnac.calculate_features.calculate_features_multi",
+            params={"export_all_neurons": True},
+            neuron_classes=["L2_X", "L6_Y"],
+            windows=["w0", "w1"],
+            suffix="_0",
+        ),
+        FeaturesConfig(
+            type="multi",
+            groupby=["simulation_id", "circuit_id", "neuron_class", "window"],
+            function="blueetl.external.bnac.calculate_features.calculate_features_multi",
+            params={"export_all_neurons": False},
+            neuron_classes=["L2_X", "L6_Y"],
+            windows=["w0", "w1"],
+            suffix="_1",
+        ),
+    ]
+    return FeaturesCollection(
+        features_configs=features_configs,
+        repo=repo,
+        cache_manager=repo.cache_manager,
+    )
